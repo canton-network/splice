c3aa02f0ab88f48296b399b5f8bb3a902a2240eb
Divam <dfordivam@gmail.com>
2026-03-30 10:53:12 +0900

    RewardConfig UI fixes
    Signed-off-by: Divam <dfordivam@gmail.com>

--- apps/common/frontend-test-handlers/src/mocks/helpers/amulet-config-helper.ts
+++ apps/common/frontend-test-handlers/src/mocks/helpers/amulet-config-helper.ts
@@ -227,6 +227,13 @@ export function getAmuletRulesConfig(
     featuredAppActivityMarkerAmount: null,
     optDevelopmentFundManager: null,
     externalPartyConfigStateTickDuration: null,
+    rewardConfig: {
+      mintingVersion: 'RewardVersion_FeaturedAppMarkers',
+      dryRunVersion: null,
+      batchSize: '100',
+      rewardCouponTimeToLive: { microseconds: '129600000000' },
+      appRewardCouponThreshold: '0.5',
+    },
   };
 }
 
@@ -458,7 +465,18 @@ export function getExpectedAmuletRulesConfigDiffsHTML(
       class="jsondiffpatch-unchanged"
       data-key="externalPartyConfigStateTickDuration"><div
         class="jsondiffpatch-property-name">externalPartyConfigStateTickDuration</div><div
-        class="jsondiffpatch-value"><pre>null</pre></div></li></ul></div>
+        class="jsondiffpatch-value"><pre>null</pre></div></li><li
+      class="jsondiffpatch-unchanged" data-key="rewardConfig"><div
+        class="jsondiffpatch-property-name">rewardConfig</div><div
+        class="jsondiffpatch-value"><pre>{
+  "mintingVersion": "RewardVersion_FeaturedAppMarkers",
+  "dryRunVersion": null,
+  "batchSize": "100",
+  "rewardCouponTimeToLive": {
+    "microseconds": "129600000000"
+  },
+  "appRewardCouponThreshold": "0.5"
+}</pre></div></li></ul></div>
   `;
   return mock;
 }
