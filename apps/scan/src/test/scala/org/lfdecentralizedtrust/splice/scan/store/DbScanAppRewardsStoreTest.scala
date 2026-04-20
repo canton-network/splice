b7e677efe73b725c004eff9dd6d7143a2c83ee02
Tim Emiola <adetokunbo@emio.la>
2026-04-20 16:31:54 +0900

    Remove the placeholder RewardComputationInputs
    Signed-off-by: Tim Emiola <adetokunbo@emio.la>

--- apps/scan/src/test/scala/org/lfdecentralizedtrust/splice/scan/store/DbScanAppRewardsStoreTest.scala
+++ apps/scan/src/test/scala/org/lfdecentralizedtrust/splice/scan/store/DbScanAppRewardsStoreTest.scala
@@ -5,7 +5,6 @@ import com.digitalasset.canton.tracing.TraceContext
 import com.digitalasset.canton.resource.DbStorage
 import com.digitalasset.canton.lifecycle.FutureUnlessShutdown
 import org.lfdecentralizedtrust.splice.migration.DomainMigrationInfo
-import org.lfdecentralizedtrust.splice.scan.automation.RewardComputationTrigger
 import org.lfdecentralizedtrust.splice.scan.rewards.{RewardComputationInputs, RewardIssuanceParams}
 import org.lfdecentralizedtrust.splice.scan.store.db.{
   DbAppActivityRecordStore,
@@ -526,10 +525,13 @@ class DbScanAppRewardsStoreTest
           Seq("bob::provider"),
           Seq(500000L),
         )
+        zeroThresholdInputs = testInputs.copy(
+          appRewardCouponThreshold = RewardComputationInputs.zero
+        )
         summary <- store.computeAndStoreRewards(
           roundNumber,
           batchSize = 100,
-          RewardComputationTrigger.placeholderInputs,
+          zeroThresholdInputs,
         )
       } yield {
         summary.activePartiesCount shouldBe 2L
@@ -546,7 +548,7 @@ class DbScanAppRewardsStoreTest
         summary <- store.computeAndStoreRewards(
           roundNumber,
           batchSize = 100,
-          RewardComputationTrigger.placeholderInputs,
+          testInputs,
         )
       } yield {
         summary.activePartiesCount shouldBe 0L
@@ -1199,7 +1201,7 @@ object DbScanAppRewardsStoreTest {
       tickDurationMicros = tickDurationMicros,
       amuletPrice = n(BigDecimal("1.0")),
       trafficPrice = n(BigDecimal("1.0")),
-      appRewardCouponThreshold = n(BigDecimal("0.5")),
+      appRewardCouponThreshold = n(BigDecimal("0.0")),
     )
   }
 
