eb50c00463963aea27c57e1cec61cc7ee5ed8ee5
Paweł Perek <pawel.perek@digitalasset.com>
2026-05-05 20:45:50 +0000

    setup oidc conformant Auth0 config [ci]
    Signed-off-by: Paweł Perek <pawel.perek@digitalasset.com>

--- cluster/pulumi/infra/src/auth0.ts
+++ cluster/pulumi/infra/src/auth0.ts
@@ -226,6 +226,16 @@ function newUiApp(
       webOrigins: urls,
       crossOriginAuth: false,
       description: ` ** Managed by Pulumi, do not edit manually **\n${description}`,
+      oidcConformant: true,
+      refreshToken: {
+        rotationType: 'rotating',
+        expirationType: 'expiring',
+        tokenLifetime: 2592000, // 30d
+        idleTokenLifetime: 1296000, // 15d
+        infiniteTokenLifetime: false,
+        infiniteIdleTokenLifetime: false,
+        leeway: 5,
+      },
     },
     { provider: auth0DomainProvider }
   );
