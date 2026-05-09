15b2f19864b0c009c82ecc4e933f6770d7257f1c
Itai Segall <itai.segall@digitalasset.com>
2026-05-09 00:17:12 +0000

    [static] copyright
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- scripts/check-base-images.py
+++ scripts/check-base-images.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from dockerfile_parse import DockerfileParser
 import sys
 import subprocess
