e890cf01fc4c5f7831b00ce1b53cd8b5ef5e2613
Stanislav German-Evtushenko <ginermail@gmail.com>
2026-05-14 00:36:22 +0900

    helm, splice-info: Add license headers to shell scripts

--- cluster/helm/splice-info/scripts/get-status.sh
+++ cluster/helm/splice-info/scripts/get-status.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 set -euo pipefail
 
 SV_METRICS_URL="${SV_METRICS_URL:-http://sv-app:10013/metrics}"
--- cluster/helm/splice-info/scripts/updater.sh
+++ cluster/helm/splice-info/scripts/updater.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 set -eu
 
 period=60
