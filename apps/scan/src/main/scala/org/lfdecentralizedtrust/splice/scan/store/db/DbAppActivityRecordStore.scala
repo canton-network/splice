737a64fae6ef9ef161ff003b2e46caf1c6d1a9d5
Tim Emiola <adetokunbo@emio.la>
2026-05-13 21:25:24 +0900

    Delete ActivityIngestionMetaCheck; test ensureMetaDBIO directly
    Signed-off-by: Tim Emiola <adetokunbo@emio.la>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbAppActivityRecordStore.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbAppActivityRecordStore.scala
@@ -414,17 +414,6 @@ class DbAppActivityRecordStore(
 
   private val metaChecked = new AtomicBoolean(false)
 
-  /** Future-returning wrapper around [[ensureMetaDBIO]]. */
-  def ensureMeta(
-      ingestionStart: Option[(Long, Long)]
-  )(implicit tc: TraceContext): Future[EnsureResult] =
-    futureUnlessShutdownToFuture(
-      storage.queryAndUpdate(
-        ensureMetaDBIO(ingestionStart),
-        "appActivity.ensureMeta",
-      )
-    )
-
   /** DBIO action that checks/inserts the meta row.
     * Composable in a transaction with activity record inserts.
     *
