4a0802dd8f30b9437bb957de478731c6dfce60aa
Itai Segall <itai.segall@digitalasset.com>
2026-05-21 19:38:55 +0000

    [static] manage more gha secrets in pulumi
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- cluster/pulumi/gha/src/github.ts
+++ cluster/pulumi/gha/src/github.ts
@@ -2,8 +2,27 @@
 // SPDX-License-Identifier: Apache-2.0
 import * as github from '@pulumi/github';
 import { DockerConfig } from '@lfdecentralizedtrust/splice-pulumi-common/src/dockerConfig';
+import { getSecretVersionOutput } from '@pulumi/gcp/secretmanager/getSecretVersion';
 
 import { ghaConfig } from './config';
+
+function copySecretToGithubActionsSecret(
+  secretName: string,
+  githubSecretName: string,
+  repo: string,
+  provider: github.Provider
+): void {
+  const secret = getSecretVersionOutput({ secret: secretName });
+  new github.ActionsSecret(
+    `${secretName}-${repo}`,
+    {
+      repository: repo,
+      secretName: githubSecretName,
+      value: secret.secretData,
+    },
+    { provider }
+  );
+}
 
 export function installGithubRepo(repo: string): void {
   const orgProvider = new github.Provider('canton-network-provider', {
@@ -30,5 +49,38 @@ export function installGithubRepo(repo: string): void {
       value: creds.apply(creds => creds.password),
     },
     { provider: orgProvider }
+  );
+
+  const auth0TestsManagementApi = getSecretVersionOutput({ secret: 'auth0-tests-management-api' });
+  new github.ActionsSecret(
+    `auth0-tests-management-api-client-id-${repo}`,
+    {
+      repository: repo,
+      secretName: 'AUTH0_TESTS_MANAGEMENT_API_CLIENT_ID',
+      value: auth0TestsManagementApi.apply(api => JSON.parse(api.secretData).clientId),
+    },
+    { provider: orgProvider }
+  );
+  new github.ActionsSecret(
+    `auth0-tests-management-api-client-secret-${repo}`,
+    {
+      repository: repo,
+      secretName: 'AUTH0_TESTS_MANAGEMENT_API_CLIENT_SECRET',
+      value: auth0TestsManagementApi.apply(api => JSON.parse(api.secretData).secret),
+    },
+    { provider: orgProvider }
+  );
+
+  copySecretToGithubActionsSecret(
+    'auth0-validator-audience',
+    'AUTH0_VALIDATOR_AUDIENCE',
+    repo,
+    orgProvider
+  );
+  copySecretToGithubActionsSecret(
+    'compose-validator-web-ui-password',
+    'COMPOSE_VALIDATOR_WEB_UI_PASSWORD',
+    repo,
+    orgProvider
   );
 }
