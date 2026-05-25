87d91d4f25c35e21b3a81c0b9c92790f11d7bba4
Tim Emiola <adetokunbo@emio.la>
2026-05-25 13:27:05 +0900

    Perform DB-level ordering of the fetched CalculateV2Rewards contracts
    Signed-off-by: Tim Emiola <adetokunbo@emio.la>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbScanRewardsReferenceStore.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbScanRewardsReferenceStore.scala
@@ -18,7 +18,7 @@ import org.lfdecentralizedtrust.splice.config.IngestionConfig
 import org.lfdecentralizedtrust.splice.environment.RetryProvider
 import org.lfdecentralizedtrust.splice.migration.DomainMigrationInfo
 import org.lfdecentralizedtrust.splice.scan.store.ScanRewardsReferenceStore
-import org.lfdecentralizedtrust.splice.store.{Limit, TcsStore}
+import org.lfdecentralizedtrust.splice.store.{Limit, LimitHelpers, TcsStore}
 import org.lfdecentralizedtrust.splice.store.db.{
   AcsArchiveConfig,
   AcsQueries,
@@ -77,7 +77,8 @@ class DbScanRewardsReferenceStore(
     )
     with ScanRewardsReferenceStore
     with AcsTables
-    with AcsQueries {
+    with AcsQueries
+    with LimitHelpers {
 
   override def waitUntilInitialized: Future[Unit] = multiDomainAcsStore.waitUntilAcsIngested()
 
@@ -220,9 +221,20 @@ class DbScanRewardsReferenceStore(
   override def listActiveCalculateRewardsV2(limit: Limit = defaultLimit)(implicit
       tc: TraceContext
   ): Future[Seq[Contract[CalculateRewardsV2.ContractId, CalculateRewardsV2]]] =
-    waitUntilInitialized.flatMap { _ =>
-      multiDomainAcsStore
-        .listContracts(CalculateRewardsV2.COMPANION, limit)
-        .map(_.map(_.contract).sortBy(_.payload.round.number))
-    }
+    for {
+      _ <- waitUntilInitialized
+      result <- futureUnlessShutdownToFuture(
+        storage.query(
+          selectFromAcsTable(
+            ScanRewardsReferenceTables.acsTableName,
+            multiDomainAcsStore.acsStoreId,
+            multiDomainAcsStore.domainMigrationId,
+            CalculateRewardsV2.COMPANION,
+            orderLimit = sql"""order by acs.round asc limit ${sqlLimit(limit)}""",
+          ),
+          "listActiveCalculateRewardsV2",
+        )
+      )
+      limited = applyLimit("listActiveCalculateRewardsV2", limit, result)
+    } yield limited.map(contractFromRow(CalculateRewardsV2.COMPANION)(_))
 }
