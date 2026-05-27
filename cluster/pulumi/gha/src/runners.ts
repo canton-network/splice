5030bead8345c148b84a19e277f7a035cda5309b
Itai Segall <itai.segall@digitalasset.com>
2026-05-27 19:06:56 +0000

    [static] fmt

--- cluster/pulumi/gha/src/runners.ts
+++ cluster/pulumi/gha/src/runners.ts
@@ -305,7 +305,7 @@ function installDockerRunnerScaleSets(
     .filter(spec => spec.docker)
     .forEach(spec => {
       installDockerRunnerScaleSet(
-        (repo == 'splice' ? `self-hosted-docker-${spec.name}` : `docker-${spec.name}`),
+        repo == 'splice' ? `self-hosted-docker-${spec.name}` : `docker-${spec.name}`,
         runnersNamespace,
         controller,
         tokenSecret,
@@ -673,7 +673,7 @@ function installK8sRunnerScaleSets(
     .forEach(spec => {
       installK8sRunnerScaleSet(
         runnersNamespace,
-        (repo == 'splice' ? `self-hosted-k8s-${spec.name}` : `k8s-${spec.name}`),
+        repo == 'splice' ? `self-hosted-k8s-${spec.name}` : `k8s-${spec.name}`,
         tokenSecret,
         cachePvcName,
         spec.resources,
