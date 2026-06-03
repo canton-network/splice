beef2b611cb74294d1e8d21ca5d9a2971a8f02fc
krzysztofczyz-da <krzysztof.czyz@digitalasset.com>
2026-06-03 14:12:29 +0000

    Update nix&pulumi

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,21 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    # Pinned to the last nixpkgs-unstable commit before google-cloud-sdk switched its
+    # bundled Python from 3.12 to 3.14
+    nixpkgs-for-gcloud.url = "github:NixOS/nixpkgs/7e694d87970c8a280ac5420a5af2738a63ed2711";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-for-gcloud, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let enterprise_pkgs = import nixpkgs { inherit system; overlays = import ./overlays.nix { use_enterprise = true; }; };
-            oss_pkgs = import nixpkgs { inherit system; overlays = import ./overlays.nix { use_enterprise = false; }; };
+        let pkgs-for-gcloud = import nixpkgs-for-gcloud { inherit system; };
+            enterprise_pkgs = import nixpkgs { inherit system; overlays = import ./overlays.nix { use_enterprise = true; inherit pkgs-for-gcloud; }; };
+            oss_pkgs = import nixpkgs { inherit system; overlays = import ./overlays.nix { use_enterprise = false; inherit pkgs-for-gcloud; }; };
             enterprise_x86Pkgs =
               if system == "aarch64-darwin"
-              then import nixpkgs { system = "x86_64-darwin"; overlays = import ./overlays.nix { use_enterprise = true; }; }
+              then import nixpkgs { system = "x86_64-darwin"; overlays = import ./overlays.nix { use_enterprise = true; pkgs-for-gcloud = import nixpkgs-for-gcloud { system = "x86_64-darwin"; }; }; }
               else enterprise_pkgs;
             oss_x86Pkgs =
               if system == "aarch64-darwin"
-              then import nixpkgs { system = "x86_64-darwin"; overlays = import ./overlays.nix { use_enterprise = false; }; }
+              then import nixpkgs { system = "x86_64-darwin"; overlays = import ./overlays.nix { use_enterprise = false; pkgs-for-gcloud = import nixpkgs-for-gcloud { system = "x86_64-darwin"; }; }; }
               else oss_pkgs;
 
         in
