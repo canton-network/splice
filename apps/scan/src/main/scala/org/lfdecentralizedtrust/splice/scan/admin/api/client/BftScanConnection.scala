4717f9462a01404a73b19fa166ce869e7e7a0406
Divam <dfordivam@gmail.com>
2026-06-05 03:25:13 +0000

    Change logic to consider http failure in quorum
    Signed-off-by: Divam <dfordivam@gmail.com>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/admin/api/client/BftScanConnection.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/admin/api/client/BftScanConnection.scala
@@ -858,11 +858,11 @@ object BftScanConnection {
               (_, scans) => scan.url :: Option(scans).getOrElse(List.empty),
             )
 
-          // In the special case of nTargetSuccess == 1, consider the response only if it is a success
+          // In the special case of nTargetSuccess == 1, ignore error responses
           // Otherwise a single HTTP error or network failure would prevent reading the responses from others
           val considerResponseForQuorum = key match {
-            case _: SuccessfulResponse[?] => true
-            case _ => !(nTargetSuccess == 1 && requestFrom.size != 1)
+            case _: ExceptionFailureResponse[?] => !(nTargetSuccess == 1 && requestFrom.size != 1)
+            case _ => true
           }
           if (considerResponseForQuorum && agreements.size == nTargetSuccess) { // consensus has been reached
             finalResponse.tryComplete(response): Unit
