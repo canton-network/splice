296f344e56d618f3b075605e5f4203bfeef35853
moritzkiefer-da <45630097+moritzkiefer-da@users.noreply.github.com>
2026-06-12 15:11:13 +0200

    Reconcile bft sequencing parameters (#5934)
    fixes #5147
    
    [ci]
    
    Signed-off-by: moritz.kiefer@digitalasset.com <moritz.kiefer@purelyfunctional.org>
    Co-authored-by: moritz.kiefer@digitalasset.com <moritz.kiefer@purelyfunctional.org>

--- apps/app/src/main/scala/org/lfdecentralizedtrust/splice/config/SpliceConfig.scala
+++ apps/app/src/main/scala/org/lfdecentralizedtrust/splice/config/SpliceConfig.scala
@@ -635,6 +635,8 @@ object SpliceConfig {
       deriveReader[RangeConfig]
     implicit val packageVettingCacheConfig: ConfigReader[PackageVettingLookupService.CacheConfig] =
       deriveReader[PackageVettingLookupService.CacheConfig]
+    implicit val sequencingParametersReader: ConfigReader[BftSequencingParameters] =
+      deriveReader[BftSequencingParameters]
     implicit val svConfigReader: ConfigReader[SvAppBackendConfig] =
       deriveReader[SvAppBackendConfig].emap { conf =>
         def checkFoundDsoConfig(check: (SvAppBackendConfig, FoundDso) => Boolean) =
@@ -1108,6 +1110,8 @@ object SpliceConfig {
       deriveWriter[RangeConfig]
     implicit val packageVettingCacheConfig: ConfigWriter[PackageVettingLookupService.CacheConfig] =
       deriveWriter[PackageVettingLookupService.CacheConfig]
+    implicit val sequencingParametersWriter: ConfigWriter[BftSequencingParameters] =
+      deriveWriter[BftSequencingParameters]
     implicit val svConfigWriter: ConfigWriter[SvAppBackendConfig] =
       deriveWriter[SvAppBackendConfig]
 
