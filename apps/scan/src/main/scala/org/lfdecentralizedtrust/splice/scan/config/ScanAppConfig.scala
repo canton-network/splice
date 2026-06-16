772887a4ab31d780f85f1285333423b9519b244e
Oriol Muñoz <oriol.munoz@digitalasset.com>
2026-06-16 15:44:37 +0000

    scalafmt [ci]
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/config/ScanAppConfig.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/config/ScanAppConfig.scala
@@ -83,7 +83,8 @@ case class ScanAppBackendConfig(
     txLogStoreDescriptorUserVersion: Option[Long] = None,
     activityIngestionUserVersion: Option[Long] = None,
     bulkStorage: BulkStorageConfig = BulkStorageConfig(),
-    tokenStandardSettlement: TokenStandardConfig.SettlementConfig = TokenStandardConfig.SettlementConfig(),
+    tokenStandardSettlement: TokenStandardConfig.SettlementConfig =
+      TokenStandardConfig.SettlementConfig(),
     publicUrl: Option[Uri] = None,
     // The thresholdDate from which external transaction hashes are included in the updates from internal ScanAPIs.
     // TODO(#4249): use on-ledger synchronization for switching record times
--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/config/TokenStandardConfig.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/config/TokenStandardConfig.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package org.lfdecentralizedtrust.splice.scan.config
 
 import org.lfdecentralizedtrust.splice.codegen.java.splice.api.token.allocationinstructionv2
