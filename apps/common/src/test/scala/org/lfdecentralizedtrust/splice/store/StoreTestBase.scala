9f60f02f2d8238d64709a1092bfe75b107ad2db0
Tim Emiola <adetokunbo@emio.la>
2026-06-17 12:40:40 +0900

    [ci] Fix ExternalPartyWalletStore to ingest beneficiary AppRewardCoupons
    Signed-off-by: Tim Emiola <adetokunbo@emio.la>

--- apps/common/src/test/scala/org/lfdecentralizedtrust/splice/store/StoreTestBase.scala
+++ apps/common/src/test/scala/org/lfdecentralizedtrust/splice/store/StoreTestBase.scala
@@ -542,6 +542,7 @@ abstract class StoreTestBase
       provider: PartyId,
       featured: Boolean = false,
       amount: Numeric.Numeric = numeric(1.0),
+      beneficiary: Option[PartyId] = None,
       contractId: String = nextCid(),
   ): Contract[amuletCodegen.AppRewardCoupon.ContractId, amuletCodegen.AppRewardCoupon] =
     contract(
@@ -553,7 +554,7 @@ abstract class StoreTestBase
         featured,
         amount,
         new Round(round),
-        Optional.empty(),
+        beneficiary.map(_.toProtoPrimitive).fold(Optional.empty[String]())(Optional.of),
       ),
     )
 
