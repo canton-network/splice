5aad12a217f84d18824349f45c51256b9b265abb
canton-network-da <team-canton-network@digitalasset.com>
2026-06-19 09:39:17 +0200

    Backport PR #6046 to release-line-0.6.7 (#6053)
    * [static] pulumi: support transfer preapprovals in sweeps (#6046)
    
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>
    
    * [static]
    
    Signed-off-by: DA Automation <splice-maintainers@digitalasset.com>
    
    ---------
    
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>
    Signed-off-by: DA Automation <splice-maintainers@digitalasset.com>
    Co-authored-by: Itai Segall <itai.segall@digitalasset.com>
    Co-authored-by: DA Automation <splice-maintainers@digitalasset.com>

--- cluster/pulumi/common-validator/src/validator.ts
+++ cluster/pulumi/common-validator/src/validator.ts
@@ -171,6 +171,7 @@ export async function installValidatorApp(
       maxBalanceUSD: config.sweep.maxBalance,
       minBalanceUSD: config.sweep.minBalance,
       receiver: config.sweep.toParty,
+      useTransferPreapproval: config.sweep.useTransferPreapproval,
     },
   };
 
