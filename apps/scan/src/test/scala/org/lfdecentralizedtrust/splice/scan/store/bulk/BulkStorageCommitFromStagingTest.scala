bd4630fb79d7395f6485ab45a18b57e8e1397a9a
Itai Segall <itai.segall@digitalasset.com>
2026-06-22 17:16:00 +0000

    [ci] test 2
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- apps/scan/src/test/scala/org/lfdecentralizedtrust/splice/scan/store/bulk/BulkStorageCommitFromStagingTest.scala
+++ apps/scan/src/test/scala/org/lfdecentralizedtrust/splice/scan/store/bulk/BulkStorageCommitFromStagingTest.scala
@@ -4,9 +4,11 @@
 package org.lfdecentralizedtrust.splice.scan.store.bulk
 
 import com.digitalasset.canton.lifecycle.FutureUnlessShutdown
+import com.digitalasset.canton.logging.SuppressionRule
 import com.digitalasset.canton.resource.DbStorage
 import com.digitalasset.canton.tracing.TraceContext
 import com.digitalasset.canton.{HasActorSystem, HasExecutionContext}
+import org.slf4j.event.Level
 import org.apache.pekko.stream.scaladsl.Keep
 import org.apache.pekko.stream.testkit.scaladsl.{TestSink, TestSource}
 import org.lfdecentralizedtrust.splice.store.S3BucketConnection.ObjectKeyAndChecksum
@@ -31,53 +33,83 @@ class BulkStorageCommitFromStagingTest
   "BulkStorageCommitFromStaging" should {
     "successfully move objects from staging to committed S3 bucket" in {
 
-      val stagingS3Connection =
-        new S3BucketConnectionForUnitTests(s3ConfigMock("staging"), loggerFactory)
-      val committedS3Connection =
-        new S3BucketConnectionForUnitTests(s3ConfigMock("committed"), loggerFactory)
+      val (stagingS3Connection, committedS3Connection, objsWithDigests) = setupTest
 
-      def createStagingObject(content: String) = {
-        val md = MessageDigest.getInstance("SHA-256")
-        md.update(content.getBytes)
-        val digest = Base64.getEncoder.encodeToString(md.digest())
-        val key = s"$content.txt"
-        stagingS3Connection.createObject(key, content.getBytes).futureValue
-        ObjectKeyAndChecksum(key, digest)
-      }
+      triggerCopyFlow(stagingS3Connection, committedS3Connection, objsWithDigests)
 
-      val objsWithDigests =
-        Seq("object1", "object2", "object3").map(createStagingObject)
+      assertObjectsMoved(stagingS3Connection, committedS3Connection, objsWithDigests)
+    }
 
-      val flow = processFlow(
-        stagingS3Connection,
-        committedS3Connection,
-        _ => Future.successful(objsWithDigests),
+    "skip previously copied objects" in {
+      val (stagingS3Connection, committedS3Connection, objsWithDigests) = setupTest
+
+      // Pre-copy one object to the committed bucket
+      val preCopiedObject = objsWithDigests.head
+      committedS3Connection.copyObject(stagingS3Connection.bucketName, preCopiedObject.key).futureValue
+
+      loggerFactory.assertLogsSeq(SuppressionRule.LevelAndAbove(Level.DEBUG))(
+        {
+          triggerCopyFlow(stagingS3Connection, committedS3Connection, objsWithDigests)
+        },
+        logEntries =>
+          forExactly(1, logEntries)(_.message should include("Skipping copy")),
       )
 
-      val (pub, sub) = TestSource
-        .probe[String]
-        .via(flow)
-        .toMat(TestSink.probe[String])(Keep.both)
-        .run()
-
-      sub.request(1)
-      pub.sendNext("go")
-      sub.expectNext("go")
-
-      clue("All objects have been moved from staging to committed S3 bucket") {
-        stagingS3Connection.listObjects.futureValue.contents().asScala shouldBe empty
-        committedS3Connection.listObjects.futureValue
-          .contents()
-          .asScala
-          .map(_.key()) should contain theSameElementsAs objsWithDigests.map(_.key)
-      }
-
-      clue("Checksums have been preserved in the committed S3 bucket") {
-        committedS3Connection
-          .getChecksums(objsWithDigests.map(_.key))
-          .futureValue should contain theSameElementsAs objsWithDigests
-      }
+      assertObjectsMoved(stagingS3Connection, committedS3Connection, objsWithDigests)
     }
+  }
+
+  private def triggerCopyFlow(stagingS3Connection: S3BucketConnectionForUnitTests, committedS3Connection: S3BucketConnectionForUnitTests, objsWithDigests: Seq[ObjectKeyAndChecksum]) = {
+    val flow = processFlow(
+      stagingS3Connection,
+      committedS3Connection,
+      _ => Future.successful(objsWithDigests),
+    )
+
+    val (pub, sub) = TestSource
+      .probe[String]
+      .via(flow)
+      .toMat(TestSink.probe[String])(Keep.both)
+      .run()
+
+    sub.request(1)
+    pub.sendNext("go")
+    sub.expectNext("go")
+  }
+
+  private def assertObjectsMoved(stagingS3Connection: S3BucketConnectionForUnitTests, committedS3Connection: S3BucketConnectionForUnitTests, objsWithDigests: Seq[ObjectKeyAndChecksum]) = {
+    clue("All objects have been moved from staging to committed S3 bucket") {
+      stagingS3Connection.listObjects.futureValue.contents().asScala shouldBe empty
+      committedS3Connection.listObjects.futureValue
+        .contents()
+        .asScala
+        .map(_.key()) should contain theSameElementsAs objsWithDigests.map(_.key)
+    }
+    clue("Checksums of objects in committed S3 bucket match the expected digests") {
+      committedS3Connection
+        .getChecksums(objsWithDigests.map(_.key))
+        .futureValue should contain theSameElementsAs objsWithDigests
+    }
+  }
+
+  private def setupTest = {
+    val stagingS3Connection =
+      new S3BucketConnectionForUnitTests(s3ConfigMock("staging"), loggerFactory)
+    val committedS3Connection =
+      new S3BucketConnectionForUnitTests(s3ConfigMock("committed"), loggerFactory)
+
+    def createStagingObject(content: String) = {
+      val md = MessageDigest.getInstance("SHA-256")
+      md.update(content.getBytes)
+      val digest = Base64.getEncoder.encodeToString(md.digest())
+      val key = s"$content.txt"
+      stagingS3Connection.createObject(key, content.getBytes).futureValue
+      ObjectKeyAndChecksum(key, digest)
+    }
+
+    val objsWithDigests =
+      Seq("object1", "object2", "object3").map(createStagingObject)
+    (stagingS3Connection, committedS3Connection, objsWithDigests)
   }
 
   override protected def cleanDb(
