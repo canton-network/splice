839e0ef5e439e5a91ff7ad4bb125ff63a24751a7
Nicu Reut <nicu.reut@digitalasset.com>
2026-06-30 14:08:57 +0000

    also handle backup/restore
    [static]
    
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/scripts/node-backup.sh
+++ cluster/scripts/node-backup.sh
@@ -274,6 +274,21 @@ function main() {
   local migration_id=$3
   local requested_component="${4:-}"
 
+  # Get resolved config and extract hyperdisk support flag
+  local config
+  config=$(get_resolved_config)
+  local hyperdisk_enabled
+  hyperdisk_enabled=$(echo "$config" | yq '.cluster.hyperdiskSupport.enabled // false')
+
+  # Determine whether the BFT sequencer is enabled for the migration being backed up.
+  local bft_sequencer_enabled
+  bft_sequencer_enabled=$(echo "$config" | yq "
+    ([.synchronizerMigration.active, .synchronizerMigration.upgrade, .synchronizerMigration.legacy]
+      + (.synchronizerMigration.archived // [])
+      + (.synchronizerMigration.additionalLegacy // []))
+    | map(select(.id == $migration_id))
+    | .[0].sequencer.enableBftSequencer // false")
+
   # TODO(#9361): support multiple domains / non-default-ID'd ones
   if [ "$1" == "validator" ]; then
     _info "Backing up validator $namespace"
@@ -285,20 +300,26 @@ function main() {
   elif [ "$1" == "sv" ]; then
     _info "Backing up SV node $namespace"
 
-    backup_component "$namespace" "cn-apps" "$requested_component" "$migration_id"
-    backup_component "$namespace" "mediator" "$requested_component" "$migration_id"
-    backup_component "$namespace" "sequencer" "$requested_component" "$migration_id"
-    backup_component "$namespace" "cometbft-$migration_id" "$requested_component" "$migration_id"
+    backup_component "$namespace" "cn-apps" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    backup_component "$namespace" "mediator" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    backup_component "$namespace" "sequencer" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    if [ "$bft_sequencer_enabled" == "true" ]; then
+      _info "BFT sequencer is enabled for migration $migration_id, skipping CometBFT backup"
+    else
+      backup_component "$namespace" "cometbft-$migration_id" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    fi
 
     wait_for_backup "$namespace" "cn-apps" "$requested_component" "$migration_id"
 
     # CN apps must be strictly before participant, so we sync on apps before starting the participant backup
     backup_component "$namespace" "participant" "$requested_component" "$migration_id"
 
-    wait_for_backup "$namespace" "participant" "$requested_component" "$migration_id"
-    wait_for_backup "$namespace" "mediator" "$requested_component" "$migration_id"
-    wait_for_backup "$namespace" "sequencer" "$requested_component" "$migration_id"
-    wait_for_backup "$namespace" "cometbft-$migration_id" "$requested_component" "$migration_id"
+    wait_for_backup "$namespace" "participant" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    wait_for_backup "$namespace" "mediator" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    wait_for_backup "$namespace" "sequencer" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    if [ "$bft_sequencer_enabled" != "true" ]; then
+      wait_for_backup "$namespace" "cometbft-$migration_id" "$requested_component" "$migration_id" "$hyperdisk_enabled"
+    fi
   else
     usage
     exit 1
