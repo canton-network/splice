8ec665b12f69f8846a5ca5b9e3e0a98d27eb8287
Paweł Perek <pawel.perek@digitalasset.com>
2026-07-06 13:02:17 +0000

    featured app contract ids [ci]
    Signed-off-by: Paweł Perek <pawel.perek@digitalasset.com>

--- apps/sv/frontend/src/components/forms/GrantRevokeFeaturedAppForm.tsx
+++ apps/sv/frontend/src/components/forms/GrantRevokeFeaturedAppForm.tsx
@@ -263,7 +263,9 @@ export const GrantRevokeFeaturedAppForm: React.FC<GrantRevokeFeaturedAppFormProp
                       id={`${testIdPrefix}-partyId`}
                       scrollableIdentifier
                       subtitle={
-                        field.state.meta.isValidating ? 'Loading featured app rights...' : undefined
+                        field.state.meta.isValidating
+                          ? 'Loading Featured Application Contract IDs...'
+                          : undefined
                       }
                       onChange={() => {
                         picker.resetOptions();
@@ -288,7 +290,7 @@ export const GrantRevokeFeaturedAppForm: React.FC<GrantRevokeFeaturedAppFormProp
                       disabled={picker.rightOptions.length === 0}
                       placeholder={
                         providerHasNoRights
-                          ? 'No featured application rights found for this provider'
+                          ? 'No Featured Application Contract IDs found for this provider'
                           : undefined
                       }
                     />
