62423922b556eef2f5cc85432866d1a65cabb0a5
JYC11 <jaeyoon.c@gmail.com>
2026-07-14 01:45:05 +0000

    apply comment feedback, use sealed trait for reward sharing config
    Signed-off-by: JYC11 <jaeyoon.c@gmail.com>

--- apps/wallet/src/main/scala/org/lfdecentralizedtrust/splice/wallet/UserWalletService.scala
+++ apps/wallet/src/main/scala/org/lfdecentralizedtrust/splice/wallet/UserWalletService.scala
@@ -100,7 +100,10 @@ class UserWalletService(
         walletManager,
         retryProvider,
         scanConnection,
-        mintUnassignedRewardCouponsV2 = rewardSharingConfig.beneficiaries.isEmpty && !rewardSharingConfig.isExternal,
+        mintUnassignedRewardCouponsV2 = rewardSharingConfig match {
+          case RewardSharingConfig.BuiltIn(_, beneficiaries, _) => beneficiaries.isEmpty
+          case RewardSharingConfig.External => false
+        },
         loggerFactory,
       )
     } catch {
