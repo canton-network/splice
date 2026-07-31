23f45d9f459b2ee74fbe1462edf13a76ea2a1d7c
Kajal Shah <kajal.shah@digitalasset.com>
2026-07-31 00:10:22 +0000

    added index file changes
    Signed-off-by: Kajal Shah <kajal.shah@digitalasset.com>

--- cluster/pulumi/common/src/config/index.ts
+++ cluster/pulumi/common/src/config/index.ts
@@ -14,8 +14,26 @@ export const DeployValidatorRunbook = spliceEnvConfig.envFlag(
   'SPLICE_DEPLOY_VALIDATOR_RUNBOOK',
   false
 );
-
 export const clusterProdLike = spliceEnvConfig.envFlag('GCP_CLUSTER_PROD_LIKE');
+
+
+// Datastream pipeline inclusion flags
+export const enableLegacyDatastream = spliceEnvConfig.envFlag('ENABLE_LEGACY_DATASTREAM', true);
+export const enableStagProdDatastream = spliceEnvConfig.envFlag('ENABLE_STAG_PROD_DATASTREAM', true); 
+
+// Datastream operational states ('RUNNING' or 'PAUSED')
+export const legacyDatastreamDesiredState = (
+  spliceEnvConfig.optionalEnv('LEGACY_DATASTREAM_DESIRED_STATE') ?? 'RUNNING'
+) as 'RUNNING' | 'PAUSED';
+
+export const stagProdDatastreamDesiredState = (
+  spliceEnvConfig.optionalEnv('STAG_PROD_DATASTREAM_DESIRED_STATE') ?? 'RUNNING'
+) as 'RUNNING' | 'PAUSED';
+
+// Table expiration in milliseconds for BigQuery prod dataset (defaults to 3 days: 259,200,000 ms)
+const THREE_DAYS_MS = 3 * 24 * 60 * 60 * 1000;
+const rawExpiration = spliceEnvConfig.optionalEnv('PROD_TABLE_EXPIRATION_MS');
+export const prodTableExpirationMs = rawExpiration ? Number(rawExpiration) : THREE_DAYS_MS;
 
 // During development we often overwrite the same tag so we use imagePullPolicy: Always.
 // Outside of development, we use the default which corresponds to IfNotPresent
