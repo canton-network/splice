331cbb8165a4709332ae8a8f8f8657a6e15bfb50
Martin Florian <martin.florian@digitalasset.com>
2026-08-05 13:37:10 +0000

    )
    [ci]
    
    Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/BftScanConnectionIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/BftScanConnectionIntegrationTest.scala
@@ -90,7 +90,7 @@ class BftScanConnectionIntegrationTest
         ) or
           include("Encountered 4 consecutive transient failures") or include(
             "Failed to connect to scan of FAILED Seed URL #0 (http://localhost:5112)."
-          )
+          ))
       ),
     )
 
