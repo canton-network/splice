9f74c308b9a5211fe6f082963c84352b2704d580
Oriol Muñoz <oriol.munoz@digitalasset.com>
2026-08-12 12:31:31 +0200

    Revert "Revert "[ci] Bump the development-dependencies group across 1 directo…"
    This reverts commit 905ead3cf829334795e830a2b7a00d7faa4099ea.
    
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

--- cluster/pulumi/common/src/postgres.ts
+++ cluster/pulumi/common/src/postgres.ts
@@ -509,8 +509,7 @@ export class SplicePostgres extends pulumi.ComponentResource implements Postgres
     instanceName: string,
     installPassword: (parent: Resource) => k8s.core.v1.Secret,
     splicePostgresHelmMigrationConfig:
-      | SplicePostgresMigrateConfig
-      | SplicePostgresDockerImageConfig,
+      SplicePostgresMigrateConfig | SplicePostgresDockerImageConfig,
     values?: LegacyChartValues,
     overrideDbSizeFromValues?: boolean,
     disableProtection?: boolean,
@@ -926,8 +925,7 @@ export function installSplicePostgres(
       instanceName,
       parent => installPasswordWithParent(parent, xns, instanceName, secretName),
       splicePostgresHelmMigrationConfig as
-        | SplicePostgresMigrateConfig
-        | SplicePostgresDockerImageConfig,
+        SplicePostgresMigrateConfig | SplicePostgresDockerImageConfig,
       chartValues,
       overrideDbSizeFromValues,
       opts.disableProtection,
