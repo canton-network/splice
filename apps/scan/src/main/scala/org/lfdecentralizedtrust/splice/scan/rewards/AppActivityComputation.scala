e18bc4ef6421473f09831513f77ad667ebaebe39
JYC11 <jaeyoon.c@gmail.com>
2026-08-17 04:59:08 +0000

    add round to verdict
    Signed-off-by: JYC11 <jaeyoon.c@gmail.com>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/rewards/AppActivityComputation.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/rewards/AppActivityComputation.scala
@@ -57,6 +57,13 @@ class AppActivityComputation(
       .lookupActiveOpenMiningRounds(Seq(asOf))
       .map(_.get(asOf).map { case (roundNumber, _) => roundNumber })
 
+  def lookupActiveOpenMiningRounds(
+      asOf: Seq[CantonTimestamp]
+  )(implicit tc: TraceContext): Future[Map[CantonTimestamp, Long]] =
+    rewardsReferenceStore
+      .lookupActiveOpenMiningRounds(asOf)
+      .map(_.map { case (time, (roundNumber, _)) => time -> roundNumber })
+
   /** Compute app activity records for a batch of verdicts.
     *
     * Records are returned with verdictRowId = DUMMY_VERDICT_ROW_ID as a placeholder.
