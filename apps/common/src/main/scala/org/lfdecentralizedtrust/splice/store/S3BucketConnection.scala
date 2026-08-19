d55196493783dcfaba8e7d701a40e884b9bee64b
Itai Segall <itai.segall@digitalasset.com>
2026-08-19 15:51:17 +0000

    [ci] .
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- apps/common/src/main/scala/org/lfdecentralizedtrust/splice/store/S3BucketConnection.scala
+++ apps/common/src/main/scala/org/lfdecentralizedtrust/splice/store/S3BucketConnection.scala
@@ -117,7 +117,7 @@ class S3BucketConnection(
 
   def getChecksums(
       objectKeys: Seq[String]
-  )(implicit ec: ExecutionContext, as: ActorSystem): Future[Seq[ObjectKeyAndChecksum]] = {
+  )(implicit ec: ExecutionContext, as: ActorSystem, tc: TraceContext): Future[Seq[ObjectKeyAndChecksum]] = {
     Source(objectKeys.toList)
       .mapAsync(4) { key => // TODO(#3429): make this parallelism configurable
         readChecksum(key)
@@ -127,7 +127,7 @@ class S3BucketConnection(
       .runWith(Sink.seq[ObjectKeyAndChecksum])
   }
 
-  private def readChecksum(key: String)(implicit ec: ExecutionContext): Future[Option[String]] = {
+  private def readChecksum(key: String)(implicit ec: ExecutionContext, tc: TraceContext): Future[Option[String]] = {
     val headRequest = HeadObjectRequest
       .builder()
       .bucket(bucketName)
@@ -136,7 +136,8 @@ class S3BucketConnection(
     for {
       head <- s3Client.headObject(headRequest).asScala.map(Some(_)).recover { case e =>
         // TODO(#3429): distinguish between "object not found" and other errors, probably want to catch only NoSuchKeyException, and throw everything else
-        logger.debug(s"Failed to read checksum for object $key, object may not exist: ${e.getMessage}")
+        logger
+          .debug(s"Failed to read checksum for object $key, object may not exist: ${e.getMessage}")
         None
       }
       checksum = head.map(
