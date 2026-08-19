b421f5aa327d063076d2c8c9f0e5c480a5493f66
Julien Tinguely <julien.tinguely@digitalasset.com>
2026-08-19 09:19:54 +0000

    improve alerting [static]
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>

--- cluster/pulumi/observability/src/observability.ts
+++ cluster/pulumi/observability/src/observability.ts
@@ -769,6 +769,20 @@ function substituteDsoMissedConfirmationsAlerts(alert: string): string {
     .replaceAll('$DSO_MISSED_CONFIRMATIONS_WINDOW_MINUTES', config.windowMinutes.toString());
 }
 
+function substituteSpliceRateLimitsAlerts(alert: string): string {
+  const config = monitoringConfig.alerting.alerts.spliceRateLimits;
+  const bareFilter =
+    config.excludedLimiters.length > 0 ? `limiter!~"${config.excludedLimiters.join('|')}"` : '';
+  const filter = bareFilter ? `, ${bareFilter}` : '';
+  return alert
+    .replaceAll('$SPLICE_RATE_LIMITS_USAGE_THRESHOLD', config.usageThreshold.toString())
+    .replaceAll(
+      '$SPLICE_RATE_LIMITS_REJECTION_RATE_THRESHOLD',
+      config.rejectionRateThreshold.toString()
+    )
+    .replaceAll('$SPLICE_RATE_LIMITS_FILTER', filter);
+}
+
 // AmuletMetrics was previously using owner.toString instead of owner.toProtoPrimitive
 // This function makes it compatible for both.
 function partyIdTransform(partyId: string) {
@@ -1036,8 +1050,8 @@ function createGrafanaAlerting(namespace: Input<string>) {
             'istio-rate-limiting_alerts.yaml': readGrafanaAlertingFile(
               'istio-rate-limiting_alerts.yaml'
             ),
-            'splice-rate-limiting_alerts.yaml': readGrafanaAlertingFile(
-              'splice-rate-limiting_alerts.yaml'
+            'splice-rate-limiting_alerts.yaml': substituteSpliceRateLimitsAlerts(
+              readGrafanaAlertingFile('splice-rate-limiting_alerts.yaml')
             ),
           },
         }).map(([k, v]) => [k, defaultAlertSubstitutions(v)])
