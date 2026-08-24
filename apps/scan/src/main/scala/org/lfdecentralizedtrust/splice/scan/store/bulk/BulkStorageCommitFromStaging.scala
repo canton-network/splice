376e839e09176829b6330db05696c92d792d92e7
Itai Segall <itai.segall@digitalasset.com>
2026-08-24 17:59:59 +0000

    [ci] bulk storage: improve logging on BFT disagreements
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/bulk/BulkStorageCommitFromStaging.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/bulk/BulkStorageCommitFromStaging.scala
@@ -63,7 +63,7 @@ class BulkStorageCommitFromStaging[T](
               )
             if (consensusChecksums.length == objects.length && !consensus) {
               logger.error(
-                s"All objects are known to the BFT peers, but the checksums do not match. This indicates an error in the actual data generated for bulk storage. Expected: ${objects
+                s"All objects are known to the BFT peers, but the checksums do not match, when checking objects: ${objects.map(_.key).mkString(", ")}. This indicates an error in the actual data generated for bulk storage. Expected: ${objects
                     .map(_.checksum)
                     .mkString(", ")}, got: ${consensusChecksums.mkString(", ")}"
               )
