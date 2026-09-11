566f9cfb5814644f09af22ef272a0bc5a7d276d9
Divam <681060+dfordivam@users.noreply.github.com>
2026-09-11 16:12:57 +0900

    Add PruneRewardAccountingTrigger to delete old reward-accounting data (#6874)
    Signed-off-by: Divam <dfordivam@gmail.com>

--- apps/common/src/test/scala/org/lfdecentralizedtrust/splice/store/StoreTestBase.scala
+++ apps/common/src/test/scala/org/lfdecentralizedtrust/splice/store/StoreTestBase.scala
@@ -35,7 +35,10 @@ import org.lfdecentralizedtrust.splice.codegen.java.splice.{
   schedule as scheduleCodegen,
   validatorlicense as validatorLicenseCodegen,
 }
-import org.lfdecentralizedtrust.splice.codegen.java.splice.amulet.rewardaccountingv2 as rewardAccountingCodegen
+import org.lfdecentralizedtrust.splice.codegen.java.splice.amulet.{
+  cryptohash as cryptoHashCodegen,
+  rewardaccountingv2 as rewardAccountingCodegen,
+}
 import org.lfdecentralizedtrust.splice.environment.{BaseLedgerConnection, DarResource, DarResources}
 import org.lfdecentralizedtrust.splice.environment.ledger.api.{
   ActiveContract,
@@ -363,6 +366,27 @@ abstract class StoreTestBase
     contract(
       rewardAccountingCodegen.CalculateRewardsV2.TEMPLATE_ID_WITH_PACKAGE_ID,
       new rewardAccountingCodegen.CalculateRewardsV2.ContractId(nextCid()),
+      template,
+    )
+  }
+
+  protected def processRewardsV2(
+      dso: PartyId,
+      round: Long,
+      dryRun: Boolean = true,
+      batchHash: String = "00" * 32,
+  ) = {
+    val template = new rewardAccountingCodegen.ProcessRewardsV2(
+      dso.toProtoPrimitive,
+      new Round(round),
+      Instant.now().truncatedTo(ChronoUnit.MICROS),
+      dryRun,
+      new RelTime(600_000_000L),
+      new cryptoHashCodegen.Hash(batchHash),
+    )
+    contract(
+      rewardAccountingCodegen.ProcessRewardsV2.TEMPLATE_ID_WITH_PACKAGE_ID,
+      new rewardAccountingCodegen.ProcessRewardsV2.ContractId(nextCid()),
       template,
     )
   }
