954e0c14b9d92275f3c7a607521db109e14aea68
Tim Emiola <adetokunbo@emio.la>
2026-09-14 15:56:05 +0900

    [ci] Reassign remaining redundant-version-check TODOs
    Signed-off-by: Tim Emiola <adetokunbo@emio.la>

--- apps/common/src/main/scala/org/lfdecentralizedtrust/splice/environment/PackageVersionSupport.scala
+++ apps/common/src/main/scala/org/lfdecentralizedtrust/splice/environment/PackageVersionSupport.scala
@@ -69,7 +69,6 @@ trait PackageVersionSupport extends NamedLogging {
     )
   }
 
-  // TODO(#6388): remove redundant version check
   def supportsConvertFeaturedAppActivityMarkerObservers(
       parties: Seq[PartyId],
       now: CantonTimestamp,
@@ -83,7 +82,7 @@ trait PackageVersionSupport extends NamedLogging {
       ignoreRedundantCheck = true,
     )
 
-  // TODO(#6388): remove redundant version check
+  // TODO(#1825): remove redundant version check
   def supports24hSubmissionDelay(
       amuletParties: Seq[PartyId],
       dsoGovernanceParties: Seq[PartyId],
@@ -138,7 +137,6 @@ trait PackageVersionSupport extends NamedLogging {
       tc: TraceContext
   ): Future[FeatureSupport] = supports24hSubmissionDelay(amuletParties, dsoGovernanceParties, now)
 
-  // TODO(#6388): remove redundant version check
   def supportsTrafficBasedAppRewards(parties: Seq[PartyId], now: CantonTimestamp)(implicit
       tc: TraceContext
   ): Future[FeatureSupport] =
@@ -151,7 +149,6 @@ trait PackageVersionSupport extends NamedLogging {
       ignoreRedundantCheck = true,
     )
 
-  // TODO(#6388): remove redundant version check
   def supportsMintingDelegation(parties: Seq[PartyId], now: CantonTimestamp)(implicit
       tc: TraceContext
   ): Future[FeatureSupport] =
