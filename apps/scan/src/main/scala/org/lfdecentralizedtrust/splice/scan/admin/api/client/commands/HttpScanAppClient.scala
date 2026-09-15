ead4d47cf6dd1051030db34455ce1eff1b147b5c
Itai Segall <itai.segall@digitalasset.com>
2026-09-15 02:59:38 +0000

    [ci] with unit test
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/admin/api/client/commands/HttpScanAppClient.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/admin/api/client/commands/HttpScanAppClient.scala
@@ -3,7 +3,7 @@
 
 package org.lfdecentralizedtrust.splice.scan.admin.api.client.commands
 
-import org.apache.pekko.http.scaladsl.model.{HttpHeader, HttpResponse, StatusCodes, Uri}
+import org.apache.pekko.http.scaladsl.model.{HttpHeader, HttpResponse, StatusCode, StatusCodes, Uri}
 import cats.data.EitherT
 import cats.syntax.either.*
 import cats.syntax.traverse.*
@@ -3414,6 +3414,9 @@ object HttpScanAppClient {
         http.GetBulkObjectChecksumsResponse,
         definitions.GetBulkObjectChecksumsResponse,
       ] {
+    // Keep 404 as an HTTP-level error for this call so BFT can classify "not yet" by status code.
+    override val nonErrorStatusCodes: Set[StatusCode] = Set.empty
+
     override def submitRequest(
         client: Client,
         headers: List[HttpHeader],
